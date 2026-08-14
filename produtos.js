1c9879d81e4ef7ce5cb985ef5f4cf58aca318755
Victor Oliveira de Barros <victor0105ob@gmail.com>
2026-08-13 21:14:35 -0300

    git push origin main

---
+++ produtos.js
@@ -0,0 +1,38 @@
+export const produtos = [
+    {
+        id: "1",
+        nome: "Geladeira",
+        preco: "2300,00",
+        imagem: ""
+    },
+    {
+        id: "2",
+        nome: "Sofá 3 lugares",
+        preco: "2459,00",
+        imagem: ""
+    },
+    {
+        id: "3",
+        nome: "Celular Xiaomi",
+        preco: "2189,90",
+        imagem: ""
+    },
+    {
+        id: "4",
+        nome: "Notebook",
+        preco: "4589,00",
+        imagem: ""
+    },
+    {
+        id: "5",
+        nome: "Caneca",
+        preco: "59,00",
+        imagem: ""
+    },
+    {
+        id: "6",
+        nome: "Playstation 4",
+        preco: "2229,89",
+        imagem: ""
+    }
+]
--- script2.js
+++
@@ -1,9 +0,0 @@
-function MultiplyByIndex(array)
-{
-{
-    return array.map((value, index) => value * index)
-}
-const numbers = [10, 20, 30, 40, 50];
-const result = MultiplyByIndex(numbers);
-}
-console.log(result)
